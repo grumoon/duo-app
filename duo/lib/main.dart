b81d4db248ccd48fd59e43527377d92ed43e548b
xuzhouzhang <xuzhouzhang@tencent.com>
2019-05-11 02:32:50 +0800

    get movie list

--- duo/lib/main.dart
+++ duo/lib/main.dart
@@ -1,111 +1,87 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+Future<List<Movie>> fetchMovieList() async {
+  final response = await http.post(
+      'http://www.zhangxuzhou.cn:88/movie/get_movie_list',
+      body: {"user_id": "2", "token": "5ed53a30-7349-11e9-a875-51fcd58c75e0"},
+      encoding: Utf8Codec());
+  final responseJson = json.decode(response.body);
+  final retCode = responseJson['retcode'];
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+  List<Movie> movieList = new List<Movie>();
+
+  if (retCode == 0) {
+    List jsonList = responseJson['result'];
+    jsonList.forEach((item) {
+      movieList.add(Movie.fromJson(item));
+    });
+  }
+
+  return movieList;
+}
+
+List<Widget> getWidgetList(List<Movie> movieList) {
+  List<Widget> widgetList = new List();
+  movieList.forEach((item) {
+    widgetList.add(new ListTile(
+      title: new Text(item.name),
+    ));
+  });
+  return widgetList;
+}
+
+class Movie {
+  final int movieId;
+  final String name;
+  final String audio;
+  final String subtitles;
+
+  Movie({this.movieId, this.name, this.audio, this.subtitles});
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return new Movie(
+      movieId: json['movie_id'],
+      name: json['name'],
+      audio: json['audio'],
+      subtitles: json['subtitles'],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+void main() => runApp(new MyApp());
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new MaterialApp(
+      title: 'Movie List',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Movie List'),
+        ),
+        body: new Center(
+          child: new FutureBuilder<List<Movie>>(
+            future: fetchMovieList(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return new ListView(children: getWidgetList(snapshot.data));
+              } else if (snapshot.hasError) {
+                return new Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner
+              return new CircularProgressIndicator();
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
